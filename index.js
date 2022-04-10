96b99b4559880410c771727baf6655a85441a530
Elijah Mbugi <mukundielijah@gmail.com>
2022-04-10 14:35:34 +0300

    commit tree 64d9d0ddef5afeba8f4a1c53607a31387d791ab7

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employees={
+    name:'Jane',
+    streedAddress:'12 narrowWay'
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newEmployee={...employee};
+    newEmployee[key]=value;
+    return newEmployee
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+
+}
+
+function deleteFromEmployeeByKey(){
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(){
+    
+}
